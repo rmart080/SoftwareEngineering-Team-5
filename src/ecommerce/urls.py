e03a76dca1aa7805c5feb5d042e1f8f1614e92f6
Eddy Mendez <eddymendez1904@gmail.com>
2018-06-07 17:56:40 -0400

    Adding the rest of the changes

---
+++ src/ecommerce/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+from .views import home_page, about_page, contact_page, login_page, register_page
+
+urlpatterns = [
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name = 'login'),
+    url(r'^register/$', register_page, name='register'),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^products/', include("products.urls", namespace='products')),
+    url(r'^search/', include("search.urls", namespace='search')),
+    url(r'^admin/', admin.site.urls), 
+]
+
+if settings.DEBUG:
+	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
